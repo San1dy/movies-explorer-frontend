0e6bbe97332a8dabd5175a35de6686b9a01b3323
Ivan Alekseev <obivan266@gmail.com>
2023-07-30 15:25:40 +0300

    добавлены роуты

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,17 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Main from '../Main/Main';
 
 function App() {
 	return (
-    <></>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/movies" element={<Movies />} />
+      <Route path="/saved-movies" element={<SavedMovies />} />
+      <Route path="/profile" element={<Register />} />
+      <Route path="/signin" element={<Login />} />
+      <Route path="/signup" element={<Profile />} />
+    </Routes>
   );
 }
 
