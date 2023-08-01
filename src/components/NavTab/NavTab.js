84309592b8617985c6b706a6801669dbe7688653
Ivan Alekseev <obivan266@gmail.com>
2023-08-01 15:01:32 +0300

    верстка 3.0

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,8 +1,30 @@
 import './NavTab.css';
 
 function NavTab() {
-	return (
-    <></>
+  return (
+    <section className='nav-tab'>
+      <div className='nav-tab__container'>
+        <nav>
+          <ul className='nav-tab__list'>
+            <li>
+              <a className='nav-tab__link' href='#about-project'>
+                О проекте
+              </a>
+            </li>
+            <li>
+              <a className='nav-tab__link' href='#techs'>
+                Технологии
+              </a>
+            </li>
+            <li>
+            <a className='nav-tab__link' href='#about-me'>
+                Студент
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </section>
   );
 }
 
